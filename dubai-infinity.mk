8a5a8224a23a96f7e86c25e32ebb5ad95a0528a4
Fraaxius <franthekillaz@gmail.com>
2024-08-18 11:59:29 -0400

    scripts: Let's go, Infinity-X

--- dubai-infinity.mk
+++ dubai-infinity.mk
@@ -9,9 +9,9 @@ rm -rf kernel/motorola
 rm -rf hardware/motorola
 rm -rf system/qcom
 
-git clone https://github.com/Fraaxius/android_device_motorola_dubai.git -b udc device/motorola/dubai
-git clone https://github.com/Fraaxius/android_device_motorola_sm7325-common.git -b udc device/motorola/sm7325-common
-git clone https://github.com/Fraaxius/kernel_motorola_sm7325.git -b lineage-21 kernel/motorola/sm7325
+git clone https://github.com/Fraaxius/android_device_motorola_dubai.git -b 14 device/motorola/dubai
+git clone https://github.com/Fraaxius/android_device_motorola_sm7325-common.git -b 14 device/motorola/sm7325-common
+git clone https://github.com/LineageOS/android_kernel_motorola_sm7325.git -b lineage-21 kernel/motorola/sm7325
 
 git clone https://github.com/Fraaxius/vendor_motorola_dubai.git vendor/motorola/dubai
 git clone https://github.com/Fraaxius/vendor_motorola_sm7325-common.git vendor/motorola/sm7325-common
@@ -21,5 +21,5 @@ git clone https://github.com/LineageOS/android_system_qcom.git -b lineage-21 sys
 
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 . build/envsetup.sh
-lunch lineage_dubai-userdebug
-make bacon -j$(nproc --all)
+lunch infinity_dubai-userdebug
+mka bacon -j$(nproc --all)
