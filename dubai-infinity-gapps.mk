4de4b3a5eeaf33824783ede69d066cafa12b462a
Fraaxius <franthekillaz@gmail.com>
2024-08-18 12:03:44 -0400

    scripts: Introduce Infinity-X with GApps for Dubai

---
+++ dubai-infinity-gapps.mk
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+#
+# Copyright © 2020-2021 Nemesis Team
+#
+
+rm -rf device/motorola
+rm -rf vendor/motorola
+rm -rf kernel/motorola
+rm -rf hardware/motorola
+rm -rf system/qcom
+
+git clone https://github.com/Fraaxius/android_device_motorola_dubai.git -b 14-gapps device/motorola/dubai
+git clone https://github.com/Fraaxius/android_device_motorola_sm7325-common.git -b 14-gapps device/motorola/sm7325-common
+git clone https://github.com/LineageOS/android_kernel_motorola_sm7325.git -b lineage-21 kernel/motorola/sm7325
+
+git clone https://github.com/Fraaxius/vendor_motorola_dubai.git vendor/motorola/dubai
+git clone https://github.com/Fraaxius/vendor_motorola_sm7325-common.git vendor/motorola/sm7325-common
+
+git clone https://github.com/LineageOS/android_hardware_motorola.git -b lineage-21 hardware/motorola
+git clone https://github.com/LineageOS/android_system_qcom.git -b lineage-21 system/qcom
+
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+. build/envsetup.sh
+lunch infinity_dubai-userdebug
+mka bacon -j$(nproc --all)
